9d1a0f3a63b18e05faf72ab89d226bb971084c0b
Taiga Hashimoto <taigatoratora729@yahoo.co.jp>
2022-06-26 12:11:05 +0900

    build and test on action

--- hello.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Hello World!!"
-echo "Study now..."
---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+result=$(./a.out)
+if [ "$result" = "Hello World" ]; then
+    echo "test success"
+else
+    echo "test failed"
+    exit 1
+fi
+
+exit 0
